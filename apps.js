54c06ebd07a7dc61081ed703d44fd682dd4d13b2
HugoMelin <hugo.melin01@gmail.com>
2024-03-27 19:08:43 +0100

    Add feature to add comment

--- apps.js
+++ apps.js
@@ -17,6 +17,21 @@ function ecouteTryCatch(element) {
     })
 }
 
+function createComment(firstName, lastName, message) {
+    let div1 = document.createElement("div")
+    div1.innerHTML = `<div class="flex space-x-4 text-sm text-gray-500">
+    <div class="flex-1 py-10 border-t border-gray-200">
+    <h3 class="font-medium text-gray-900">${firstName} ${lastName}</h3>
+    <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+    <p>${message}</p>
+    </div>
+    </div>
+    </div>`
+
+    let commentList = document.getElementById("comment-list")
+    commentList.appendChild(div1)
+}
+
 let firstName = document.getElementById("first-name")
 ecouteTryCatch(firstName)
 
